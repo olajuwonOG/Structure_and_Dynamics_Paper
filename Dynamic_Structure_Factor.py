a8a4d781b1be2b3020a03e202687780b313af811
Taofeek Tejuosho <olajuwon.tejuosho@yahoo.co.uk>
2024-07-04 16:56:53 -0400

    Added Static and Dynamic Structure Factor code

---
+++ Dynamic_Structure_Factor.py
@@ -0,0 +1,214 @@
+import math
+import numpy as np
+import csv
+import time
+
+# Function to read timesteps from a file
+def readTimesteps(inputFile, timestepsCount=None):
+    timesteps = {}
+    with open(inputFile) as f:
+        line = f.readline()
+        while line != '' and (not timestepsCount or len(timesteps) < timestepsCount):
+            if line.split()[0] == "ITEM:":
+                timestep = int(f.readline().split()[0])
+                timesteps[timestep] = {}
+                f.readline()
+                atoms = int(f.readline().split()[0])
+                timesteps[timestep]["atoms"] = atoms
+                f.readline()
+                x = f.readline()
+                x_pos = float(x.split()[1])
+                x_neg = float(x.split()[0])
+                timesteps[timestep]["-x"] = x_neg
+                timesteps[timestep]["x"] = x_pos
+                y = f.readline()
+                y_pos = float(y.split()[1])
+                y_neg = float(y.split()[0])
+                timesteps[timestep]["-y"] = y_neg
+                timesteps[timestep]["y"] = y_pos
+                z = f.readline()
+                z_pos = float(z.split()[1])
+                z_neg = float(z.split()[0])
+                timesteps[timestep]["-z"] = z_neg
+                timesteps[timestep]["z"] = z_pos
+                headers = f.readline()
+                timesteps[timestep]["headers"] = headers
+                timesteps[timestep]["mols"] = {}
+                for i in range(atoms):
+                    line = f.readline()
+                    id = int(line.split()[0])
+                    mol = int(line.split()[1])
+                    type = int(line.split()[2])
+                    q = int(line.split()[3])
+                    xs = float(line.split()[4]) * (timesteps[timestep]['x'] - timesteps[timestep]['-x'])
+                    ys = float(line.split()[5]) * (timesteps[timestep]['y'] - timesteps[timestep]['-y'])
+                    zs = float(line.split()[6]) * (timesteps[timestep]['z'] - timesteps[timestep]['-z'])
+                    ix = int(line.split()[7])
+                    iy = int(line.split()[8])
+                    iz = int(line.split()[9])
+                    xbox = (timesteps[timestep]['x'] - timesteps[timestep]['-x'])
+                    ybox = (timesteps[timestep]['y'] - timesteps[timestep]['-y'])
+                    zbox = (timesteps[timestep]['z'] - timesteps[timestep]['-z'])
+                    if xs > xbox:
+                        xs = xs - xbox
+                    elif xs < 0:
+                        xs = xs + xbox
+                    if ys > ybox:
+                        ys = ys - ybox
+                    elif ys < 0:
+                        ys = ys + ybox
+                    if zs > zbox:
+                        zs = zs - zbox
+                    elif zs < 0:
+                        zs = zs + zbox
+                    xs = xbox * float(line.split()[4]) + ix * xbox
+                    ys = ybox * float(line.split()[5]) + iy * ybox
+                    zs = zbox * float(line.split()[6]) + iz * zbox
+                    if mol in timesteps[timestep]["mols"]:
+                        timesteps[timestep]["mols"][mol]["atoms"].append((id, type, q, xs, ys, zs, ix, iy, iz))
+                    else:
+                        timesteps[timestep]["mols"][mol] = {}
+                        timesteps[timestep]["mols"][mol]["atoms"] = []
+                        timesteps[timestep]["mols"][mol]["atoms"].append((id, type, q, xs, ys, zs, ix, iy, iz))
+                for mol in timesteps[timestep]["mols"]:
+                    x = 0
+                    y = 0
+                    z = 0
+                    length = len(timesteps[timestep]["mols"][mol]["atoms"])
+                    timesteps[timestep]["mols"][mol]["length"] = length
+                    for i in range(length):
+                        x += timesteps[timestep]["mols"][mol]["atoms"][i][3]
+                        y += timesteps[timestep]["mols"][mol]["atoms"][i][4]
+                        z += timesteps[timestep]["mols"][mol]["atoms"][i][5]
+                    timesteps[timestep]["mols"][mol]["xcm"] = round(x / length, 4)
+                    timesteps[timestep]["mols"][mol]["ycm"] = round(y / length, 4)
+                    timesteps[timestep]["mols"][mol]["zcm"] = round(z / length, 4)
+            line = f.readline()
+    return timesteps
+
+# Function to calculate Rg^2
+def Rg2Calculator(timesteps):
+    for timestep in timesteps:
+        for mol in timesteps[timestep]["mols"]:
+            length = timesteps[timestep]["mols"][mol]["length"]
+            rg2 = 0
+            xcm = timesteps[timestep]["mols"][mol]["xcm"]
+            ycm = timesteps[timestep]["mols"][mol]["ycm"]
+            zcm = timesteps[timestep]["mols"][mol]["zcm"]
+            for i in range(length):
+                x = timesteps[timestep]["mols"][mol]["atoms"][i][3] - xcm
+                y = timesteps[timestep]["mols"][mol]["atoms"][i][4] - ycm
+                z = timesteps[timestep]["mols"][mol]["atoms"][i][5] - zcm
+                rg2 += (x**2 + y**2 + z**2)
+            timesteps[timestep]["mols"][mol]["rg2"] = round(rg2 / length, 4)
+    return timesteps
+
+# Function to write Rg^2 to a file
+def writeRg2ToFile(timesteps, outFile):
+    with open(outFile, 'w') as f:
+        f.write("rg2, molNumber, timestep\n")
+        for timestep in timesteps:
+            for mol in timesteps[timestep]["mols"]:
+                rg2 = str(timesteps[timestep]["mols"][mol]["rg2"])
+                molNumber = str(mol)
+                timestepWrite = str(timestep)
+                f.write(rg2 + ", " + molNumber + ", " + timestepWrite + "\n")
+
+# Function to write average Rg^2 to a file
+def writeRg2AverageToFile(timesteps, outFile, frames=1):
+    with open(outFile, 'w') as f:
+        f.write("rg2\n")
+        mols = {}
+        frameCount = 0
+        for timestep in timesteps:
+            if frameCount < frames:
+                for mol in timesteps[timestep]["mols"]:
+                    if mol not in mols:
+                        mols[mol] = timesteps[timestep]["mols"][mol]["rg2"]
+                    else:
+                        mols[mol] += timesteps[timestep]["mols"][mol]["rg2"]
+                frameCount += 1
+            else:
+                continue
+        sum = 0
+        for mol in mols:
+            sum += mols[mol] / frames
+        f.write('# ' + str(frames) + '\n')
+        f.write('polymer number, rg squared \n')
+        for mol in mols:
+            f.write(str(mol) + ', ' + str(round(mols[mol] / frames, 4)) + '\n')
+        avg_sum = round(sum / len(mols), 4)
+        rg_square_root = round(math.sqrt(avg_sum), 4)
+        f.write('------------------AVERAGE Square radius of gyration----------------\n')
+        f.write(str(avg_sum))
+        f.write('\n--------------Rg------------\n')
+        f.write(str(rg_square_root))
+
+# Function to calculate the coherent dynamic structure factor
+def calculate_Sqt_single_q(timesteps, q_values, chain_length_range):
+    Sqt = {q: [] for q in q_values}
+
+    initial_timestep = min(timesteps.keys())
+    initial_positions = {}
+    for mol in timesteps[initial_timestep]["mols"]:
+        initial_positions[mol] = {atom[0]: np.array(atom[3:6]) for atom in timesteps[initial_timestep]["mols"][mol]["atoms"]}
+
+    for q in q_values:
+        for timestep in sorted(timesteps.keys()):
+            Sq_timestep = 0
+            N = 0  # Initialize the number of atoms counter
+
+            for mol in timesteps[timestep]["mols"]:
+                mol_atoms = timesteps[timestep]["mols"][mol]["atoms"]
+                mol_length = timesteps[timestep]["mols"][mol]["length"]
+                
+                # Filter based on chain length range
+                if chain_length_range[0] <= mol_length <= chain_length_range[1]:
+                    N += len(mol_atoms)
+
+                    # Current positions of atoms in this molecule at current timestep
+                    r_current = np.array([atom[3:6] for atom in mol_atoms])
+                    # Initial positions of atoms in this molecule
+                    r_initial = np.array([initial_positions[mol][atom[0]] for atom in mol_atoms])
+
+                    # Compute all pairwise differences r_current - r_initial in a vectorized way
+                    r_diff = r_current[:, np.newaxis, :] - r_initial[np.newaxis, :, :]
+
+                    # Compute dot product q · (r_current - r_initial) for all pairs
+                    dot_product = np.dot(r_diff, q)
+
+                    # Compute sum of exponential terms in a vectorized way
+                    sum_exp = np.sum(np.exp(1j * dot_product))
+
+                    Sq_timestep += sum_exp.real
+
+            if N > 0:  # Avoid division by zero if no chains are in the length range
+                Sq_timestep /= N  # Normalize by the total number of atoms
+            else:
+                Sq_timestep = 0
+
+            Sqt[q].append(round(Sq_timestep, 4))
+
+    return Sqt
+
+def write_Sqt_to_file(Sqt, q_values, timesteps, outFile):
+    with open(outFile, 'w') as f:
+        # Write header
+        f.write("timestep," + ",".join(f"S(q,t) q={q}" for q in q_values) + "\n")
+        
+        # Write data for each timestep
+        for i, timestep in enumerate(sorted(timesteps.keys())):
+            f.write(f"{timestep}," + ",".join(f"{Sqt[q][i]}" for q in q_values) + "\n")
+
+# Main execution
+start_time = time.time()
+result = readTimesteps("production.dump", 500)
+q_values = [0.1, 0.2, 0.3, 0.4, 0.6, 1.0]
+chain_length_range = (330, 395) #(350, 370)  # Specify the range of chain lengths to include
+Sqt = calculate_Sqt_single_q(result, q_values, chain_length_range)
+write_Sqt_to_file(Sqt, q_values, result, "Sqt_results_single_q_1.4_360.csv")
+end_time = time.time()
+elapsed_time_seconds = end_time - start_time
+elapsed_time_minutes = elapsed_time_seconds / 60
+print(f"Execution time: {elapsed_time_minutes:.2f} minutes")
+print("DONE")
---
+++ Static_Struct_Factor.py
@@ -0,0 +1,231 @@
+import math
+import numpy as np
+import csv
+import time 
+
+start_time = time.time()
+
+def readTimesteps(inputFile, timestepsCount = None):
+	if timestepsCount:
+		counter = 0
+	else:
+		timestepsCount = 5 #999999999999999999999999
+	timesteps = {} #created an empty dictionary
+	with open(inputFile) as f:
+		line = f.readline()
+		#print(line)
+		while line != '':
+			if line.split()[0] == "ITEM:":
+				timestep = int(f.readline().split()[0]) # particular Timestep number
+				timesteps[timestep] = {}
+				filler = f.readline()
+				atoms = int(f.readline().split()[0]) #number of atoms
+				timesteps[timestep]["atoms"] = atoms #
+				filler = f.readline()
+				x = f.readline() #box dimension in x direction
+				x_pos = float(x.split()[1]) #positive x direction
+				#print(x_pos)
+				x_neg = float(x.split()[0])
+				#print("x negative is", x_neg)
+				timesteps[timestep]["-x"] = x_neg
+				timesteps[timestep]["x"] = x_pos
+				y = f.readline()
+				y_pos = float(y.split()[1])
+				y_neg = float(y.split()[0])
+				timesteps[timestep]["-y"] = y_neg
+				timesteps[timestep]["y"] = y_pos
+				z = f.readline()
+				z_pos = float(z.split()[1])
+				z_neg = float(z.split()[0])
+				timesteps[timestep]["-z"] = z_neg
+				timesteps[timestep]["z"] = z_pos
+				#print(timesteps[timestep]['z'])
+				headers = f.readline()
+				timesteps[timestep]["headers"] = headers
+				timesteps[timestep]["mols"] = {}
+				#print("The timestep is", timesteps, "\n", "\n", "\n  ")
+				#print("This is the dictionary that stores our data ", timesteps[timestep])
+				#print("\n")
+				#print("\n")
+				for i in range(atoms):
+					line = f.readline()
+					id = int(line.split()[0])
+					#print(id)
+					mol = int(line.split()[1])
+					type = int(line.split()[2])
+					q = int(line.split()[3])
+					xs = float(line.split()[4]) * (timesteps[timestep]['x']-timesteps[timestep]['-x'])
+					ys = float(line.split()[5]) * (timesteps[timestep]['y']-timesteps[timestep]['-y'])
+					zs = float(line.split()[6]) * (timesteps[timestep]['z']-timesteps[timestep]['-z'])
+					ix = int(line.split()[7])
+					iy = int(line.split()[8])
+					iz = int(line.split()[9])
+					xbox = (timesteps[timestep]['x']-timesteps[timestep]['-x'])
+					ybox = (timesteps[timestep]['y']-timesteps[timestep]['-y'])
+					zbox = (timesteps[timestep]['z']-timesteps[timestep]['-z'])
+					if xs > xbox:
+						xs = xs - xbox
+					elif xs < 0:
+						xs = xs + xbox
+					if ys > ybox:
+						ys = ys - ybox
+					elif ys < 0:
+						ys = ys + ybox
+					if zs > zbox:
+						zs = zs - zbox
+					elif zs < 0:
+						zs = zs + zbox
+					xs = xbox*float(line.split()[4]) + ix*xbox
+					ys = ybox*float(line.split()[5]) + iy*ybox
+					zs = zbox*float(line.split()[6]) + iz*zbox
+					#print("this is where the timestep", timesteps[timestep])
+					if mol in timesteps[timestep]["mols"]: #arranging the trajectory into dictionary
+						#print("this is the mol", timesteps[timestep]["mols"])
+						#print("Before",timesteps[timestep]["mols"][mol]["atoms"])
+						timesteps[timestep]["mols"][mol]["atoms"].append((id, type, q, xs, ys, zs, ix, iy, iz))
+						#print("After",timesteps[timestep]["mols"][mol]["atoms"])
+					else:
+						timesteps[timestep]["mols"][mol] = {}
+						#print("from the else",timesteps[timestep]["mols"][mol] )
+						timesteps[timestep]["mols"][mol]["atoms"] = []
+						#print("from the else",timesteps[timestep]["mols"][mol]["atoms"] )
+						timesteps[timestep]["mols"][mol]["atoms"].append((id, type, q, xs, ys, zs, ix, iy, iz))
+				for mol in timesteps[timestep]["mols"]:
+					x = 0
+					y = 0
+					z = 0
+					length = len(timesteps[timestep]["mols"][mol]["atoms"]) #Accessing the dictionary; dic:key:value:key:value
+					#print("this is the length", len(timesteps[timestep]["mols"][mol]["atoms"]))
+					timesteps[timestep]["mols"][mol]["length"] = length
+					#print("the length of a particular chain", timesteps[timestep]["mols"][mol]["length"])
+					#print(timesteps[timestep]["mols"])
+					for i in range(length):
+						x = x + timesteps[timestep]["mols"][mol]["atoms"][i][3]/length
+						y = y + timesteps[timestep]["mols"][mol]["atoms"][i][4]/length
+						z = z + timesteps[timestep]["mols"][mol]["atoms"][i][5]/length
+					timesteps[timestep]["mols"][mol]["xcm"] = x
+					timesteps[timestep]["mols"][mol]["ycm"] = y
+					timesteps[timestep]["mols"][mol]["zcm"] = z
+					#print("the length of this", timesteps[timestep]["mols"])
+			line = f.readline()
+			#print("This is the line ",line)
+			counter = counter + 1
+			if counter == timestepsCount:
+				return timesteps
+			#print("read timestep: ", timestep)
+			#print("final timestep", timesteps)
+	return timesteps
+
+#print(readTimesteps("production.dump", 1))
+# Calculate Rg2 (radius of gyration) of whatever system inputed.
+# Timesteps input MUST be object like dictionary from readTimesteps(....)
+def Rg2Calculator(timesteps):
+	for timestep in timesteps:
+		for mol in timesteps[timestep]["mols"]:
+			length = timesteps[timestep]["mols"][mol]["length"]
+			rg2 = 0
+			for i in range(length):
+				x = timesteps[timestep]["mols"][mol]["atoms"][i][3]
+				y = timesteps[timestep]["mols"][mol]["atoms"][i][4]
+				z = timesteps[timestep]["mols"][mol]["atoms"][i][5]
+				x = x - timesteps[timestep]["mols"][mol]["xcm"]
+				y = y - timesteps[timestep]["mols"][mol]["ycm"]
+				z = z - timesteps[timestep]["mols"][mol]["zcm"]
+				x2 = x ** 2
+				y2 = y ** 2
+				z2 = z ** 2
+				rg2 = ((x2 + y2 + z2)/length)  + rg2
+				squarerootrg = math.sqrt(rg2)
+				#print(squarerootrg)
+
+			timesteps[timestep]["mols"][mol]["rg2"] = rg2
+			#print("The final timesteps is ", timesteps)
+	return timesteps
+
+def writeRg2ToFile(timesteps, outFile):
+	with open(outFile, 'w') as f:
+		f.write("rg2, molNumber, timestep")
+		f.write("\n")
+		for timestep in timesteps:
+			for mol in timesteps[timestep]["mols"]:
+				rg2 = str(timesteps[timestep]["mols"][mol]["rg2"])
+				molNumber = str(mol)
+				timestepWrite = str(timestep)
+				f.write(rg2 + ", " + molNumber + ", " + timestepWrite)
+				f.write("\n")
+def writeRg2AverageToFile(timesteps, outFile, frames=1):
+    with open(outFile, 'w') as f:
+        f.write("rg2\n")
+        mols = {}
+        frameCount = 0
+        for timestep in timesteps:
+            if frameCount < frames:
+                for mol in timesteps[timestep]["mols"]:
+                    if mol not in mols:
+                        mols[mol] = timesteps[timestep]["mols"][mol]["rg2"]
+                    else:
+                        mols[mol] += timesteps[timestep]["mols"][mol]["rg2"]
+                frameCount += 1
+            else:
+                continue
+        sum = 0
+        for mol in mols:
+            sum += mols[mol] / frames
+        f.write('# ' + str(frames) + '\n')
+        f.write('polymer number, rg squared \n')
+        for mol in mols:
+            f.write(str(mol) + ', ' + str(mols[mol] / frames) + '\n')
+        sum = sum / len(mols)
+        rg_square_root = math.sqrt(sum)
+        f.write('------------------AVERAGE Square radius of gyration----------------\n')
+        f.write(str(sum))
+        f.write('\n--------------Rg------------\n')
+        f.write(str(rg_square_root))
+
+def calculate_structure_factor(timesteps, q_values, min_length=None, max_length=None):
+    structure_factors = {}
+    for timestep in timesteps:
+        for mol in timesteps[timestep]["mols"]:
+            N = timesteps[timestep]["mols"][mol]["length"]
+            if (min_length is not None and N < min_length) or (max_length is not None and N > max_length):
+                continue
+            positions = np.array([(atom[3], atom[4], atom[5]) for atom in timesteps[timestep]["mols"][mol]["atoms"]])
+            S_q = np.zeros_like(q_values, dtype=np.float64)
+            for i, q in enumerate(q_values):
+                sum_exp = np.sum(np.exp(1j * q * np.linalg.norm(positions, axis=1)))
+                S_q[i] = (1 / N) * np.abs(sum_exp) ** 2
+            if (timestep, mol) not in structure_factors:
+                structure_factors[(timestep, mol)] = S_q
+            else:
+                structure_factors[(timestep, mol)] += S_q
+    averaged_S_q = np.mean(np.array(list(structure_factors.values())), axis=0)
+    return q_values, averaged_S_q
+
+def write_structure_factor_to_csv(q_values, averaged_S_q, outFile):
+    with open(outFile, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["q", "S(q)"])
+        for q, S_q_value in zip(q_values, averaged_S_q):
+            writer.writerow([q, S_q_value])
+
+# Main execution
+input_file = "production.dump"
+output_file = "sofk_1.4_360_SPECIFIC.csv"
+q_values = np.logspace(-2, 2, 100)  # Define a range of q values
+
+result = readTimesteps(input_file, 100)
+#result = Rg2Calculator(result)
+
+# Define the chain length range for static structure factor calculation
+min_length = 350  # Example minimum chain length
+max_length = 370  # Example maximum chain length
+
+q_values, averaged_S_q = calculate_structure_factor(result, q_values, min_length, max_length)
+write_structure_factor_to_csv(q_values, averaged_S_q, output_file)
+
+end_time = time.time()
+elapsed_time_seconds = end_time - start_time
+elapsed_time_minutes = elapsed_time_seconds/60
+print(f"Execution time: {elapsed_time_minutes:.2f} minutes")
+
+print("DONE calculation")
