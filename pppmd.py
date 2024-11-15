7f9a480b005767ae933b07697a8b96c5923eb986
olajuwonOG <163355804+olajuwonOG@users.noreply.github.com>
2024-11-15 13:10:59 -0500

    Update pppmd.py

--- pppmd.py
+++ pppmd.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+
 # PPPMD: post-pocessing polymer molecular dynamics
 # Suite of tools to do post processing calculations relevant to polymers
 #
-# Modified J. Brown 2014-04-02
+# Modified by J. Brown 2014-04-02
 
 import sys
 import numpy as np
